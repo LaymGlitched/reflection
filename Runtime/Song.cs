2f67dca9b77ea5844d86350011236a036a7a6634
Dylan Day <65513977+LaymGlitched@users.noreply.github.com>
2024-07-12 17:09:22 +0100

    Add files via upload

---
+++ Runtime/Song.cs
@@ -0,0 +1,17 @@
+
+using System;
+using UnityEngine;
+
+[System.Serializable]
+public class Song
+{
+    public string Title;
+    public string Album;
+    public string Artists;
+    public string Genre;
+    public string Year;
+    [NonSerialized] // Exclude from default serialization
+    public Texture2D AlbumArt;
+    [NonSerialized] // Exclude from default serialization
+    public AudioClip track;
+}
